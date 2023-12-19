2194e488e094dba431a5a3c7d6eebed696c441f0
ecsimsw <ecsimsw@gmail.com>
2023-12-19 15:06:51 +0900

    update dependency

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ COPY . /app
 WORKDIR /app
 RUN pip3 install --no-cache-dir -r requirements.txt
 RUN apk update && apk add git
+RUN git config --global user.name "howler.bot"
+RUN git config --global user.email "howler.bot@giggle.com"
 ENV TZ Asia/Seoul
 
 CMD [ "python3", "-u", "server/main.py" ]
--- server/gitClient.py
+++ server/gitClient.py
@@ -7,8 +7,6 @@ PATH_OF_GIT_REPO = '/app/.git'
 def push(target_username):
     repo = Repo(PATH_OF_GIT_REPO)
     repo.git.add('--all')
-    repo.git.commit('-m', target_username + ' update score',
-        author="Git user <howler.bot@giggle.com>"
-    )
+    repo.git.commit('-m', target_username + ' update score')
     origin = repo.remote(name='origin')
     origin.push()
