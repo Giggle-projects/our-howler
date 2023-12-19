a5db199c818649e1789189ea65722e7967c93eff
ecsimsw <ecsimsw@gmail.com>
2023-12-19 14:48:50 +0900

    update dependency

--- server/gitClient.py
+++ server/gitClient.py
@@ -6,7 +6,7 @@ def push(target_username):
     repo = Repo(PATH_OF_GIT_REPO)
     repo.git.add('--all')
     repo.git.commit('-m', target_username + ' update score',
-        author="First Last <first.last@company.com>"
+        author="Git user <howler.bot@giggle.com>"
     )
     origin = repo.remote(name='origin')
     origin.push()
